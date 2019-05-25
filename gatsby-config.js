64de1094564a131097fa20ab277db4f404a3c015
Nastasia <nastya.nty@gmail.com>
2019-05-25 13:26:07 +0500

    GraphQL with Contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: 'My first React website',
+    description: 'Some text',
+    keywords: 'word01, word02, word03'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: '7jcfgt0u6ddl',
+        accessToken: '6JAKnL8WPEeYfIktl_wT_mmNoulGteFbhAgKef99gv4'
+      }
+    }
+  ],
 }
